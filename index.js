4b5eb058429b204d69ef5a33efbb01b2af72799d
Lidian Vilazio <lidianvilazio@hotmail.com>
2017-12-28 19:00:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+  var newObject = Object.assign({}, obj)
+  delete newObject.[key]
+  return newObject
 }
 
 
