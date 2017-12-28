074a1059a8a89f2ac08c73de6cd28fb0647aa7cf
Lidian Vilazio <lidianvilazio@hotmail.com>
2017-12-28 18:50:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
+
+
+function deleteFromObjectByKey(object, key) {
+  return Object.assign({}, object, {key})
+}
